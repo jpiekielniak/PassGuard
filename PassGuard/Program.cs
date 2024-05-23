ca5133c4f49f0140ff65aaa5b94f385e3f4264ea
jpiekielniak <jakubpiekielniak123@gmail.com>
2024-05-23 13:41:19 +0200

    init commit

---
+++ PassGuard/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+});
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "PassGuard",
+        Version = "v1"
+    });
+});
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => {
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "PassGuard v1");
+    }});
+}
+
+app.UseHttpsRedirection();
+app.Run();
+
